5e30d17fa05ab32c4c7e57a861c0479af62f74c4
Akash Shakdwipeea <ashakdwipeea@gmail.com>
2014-07-28 11:33:41 +0530

    image saved as medium blog unable to retrieve

---
+++ src/in/kyaji/web/addImage.java
@@ -0,0 +1,20 @@
+package in.kyaji.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Akash on 22-07-2014.
+ */
+public class addImage extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
---
+++ src/in/kyaji/web/displayImage.java
@@ -0,0 +1,7 @@
+package in.kyaji.web;
+
+/**
+ * Created by Akash on 22-07-2014.
+ */
+public class displayImage {
+}
